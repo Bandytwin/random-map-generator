de26ef40e2a47f88d1d6eadb480e7cdb85ea9350
unknown <tcowley8@gmail.com>
2023-01-28 10:28:41 -0700

    Add MapLayout class and example json layout

---
+++ MapLayout.py
@@ -0,0 +1,8 @@
+class MapLayout:
+    
+    def __init__(self, numPlayers):
+        self.playerCount = numPlayers
+        self.version = "1.0.1"
+        self.description = "A random random map"
+        self.name = "Random bando"
+        self.type = "random-map-generator-layout"
